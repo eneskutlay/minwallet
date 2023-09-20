a6add4097dd3f9d1fb98213d99ef4530d9b6941f
Enes Kutlay <hi@eneskutlay.dev>
2023-09-20 18:40:22 +0300

    Sketch

--- app/index.js
+++ app/index.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, StyleSheet, View } from "react-native";
+import { Title, Subtitle, Paragraph } from "../src/components/Texts";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+      <View style={styles.header}>
+        <Title>Welcome to Miniwallet</Title>
+      </View>
+      <View style={styles.body}>
+        <Subtitle>Personal Information</Subtitle>
+      </View>
+      <View style={styles.footer}>
+        <Paragraph>Footer</Paragraph>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
   },
+  header: {
+    width: "70%",
+    height: "130px",
+    alignItems: "center",
+    paddingVertical: 30,
+  },
+  body: {
+    width: "70%",
+    alignItems: "center",
+    height: "80%",
+  },
+  footer: {
+    width: "70%",
+    minHeight: "64px",
+    alignItems: "center",  },
 });
