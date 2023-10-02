394f41842635a88f38cb8a7935ffd47d22c43881
Enes Kutlay <hi@eneskutlay.dev>
2023-10-02 18:40:18 +0300

    Update index

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView, StyleSheet, View } from "react-native";
-import { Title, Paragraph, Hint } from "../src/components/Texts";
-import { TextField } from "../src/components/TextFields";
+import { Title, Hint } from "../src/components/Texts";
+import { SignDataForm } from "../src/container/SignDataForm";
 
 export default function App() {
   return (
@@ -9,20 +9,14 @@ export default function App() {
       <StatusBar style="auto" />
       <View style={styles.header}>
         <Title>Welcome to Minwallet</Title>
+        <Hint>Fill completely for best performance</Hint>
       </View>
       <View style={styles.body}>
-        <View style={styles.infoParagraph}>
-          <Hint>
-            Before you start, please fill out your information completely.
-          </Hint>
-        </View>
         <View style={styles.userData}>
-          <TextField label="Enter your name" placeholder="Your name" />
+          <SignDataForm />
         </View>
       </View>
-      <View style={styles.footer}>
-        <Paragraph>Footer</Paragraph>
-      </View>
+      <View style={styles.footer}></View>
     </SafeAreaView>
   );
 }
@@ -32,35 +26,32 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    width: "100%",
+    height: "100%",
   },
   header: {
-    width: "85%",
-    minHeight: "10%",
-    alignItems: "center",
-    paddingTop: 30,
-  },
-  body: {
-    width: "85%",
-    alignItems: "center",
-    minHeight: "80%",
-  },
-  infoParagraph: {
-    marginTop: 20,
     width: "100%",
     alignItems: "center",
-
+    height: "auto",
+    padding: 16,
+    zIndex: 1,
+  },
+  body: {
+    flex: 5,
+    width: "100%",
+    alignItems: "center",
     height: "auto",
   },
   userData: {
-    marginVertical: 20,
     width: "100%",
-
     alignItems: "center",
     height: "auto",
+    padding: 20,
   },
   footer: {
-    width: "85%",
-    minHeight: "10%",
+    flex: 1,
+    width: "100%",
     alignItems: "center",
+    height: "auto",
   },
 });
