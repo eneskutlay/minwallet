089ec7bdd5880eaf01c40312e763f03945043998
Enes Kutlay <hi@eneskutlay.dev>
2023-10-04 16:12:27 +0300

    onboarding data

---
+++ src/lib/data/formData.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    id: 1,
+    text: "Select the Language",
+    placeholder: "Language",
+  },
+  {
+    id: 2,
+    text: "Select the Currency",
+    placeholder: "Currency",
+  },
+  {
+    id: 3,
+    text: "Fill the username field",
+    placeholder: "Username",
+  },
+  {
+    id: 4,
+    text: "Fill monthly income field",
+    placeholder: "Monthly income",
+  },
+  {
+    id: 5,
+    text: "Fill monthly expense field",
+    placeholder: "Monthly expense",
+  },
+];
