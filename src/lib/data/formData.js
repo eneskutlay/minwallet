9c9bc5206c9e8408656e107fff3a5e12223a3c53
Enes Kutlay <hi@eneskutlay.dev>
2023-10-07 16:05:41 +0300

    Update localization

--- src/lib/data/formData.js
+++ src/lib/data/formData.js
@@ -1,26 +1,50 @@
 export default [
   {
     id: 1,
-    title: "Enter your username",
-    placeholder: "Username",
+    title: {
+      en: "Enter your username",
+      tr: "Kullanıcı adınızı girin",
+    },
+    placeholder: {
+      en: "Username",
+      tr: "Kullanıcı Adı",
+    },
     keyboardType: "default",
   },
   {
     id: 2,
-    title: "Enter your monthly income",
-    placeholder: "Monthly Income",
+    title: {
+      en: "Enter your monthly income",
+      tr: "Aylık gelirinizi girin",
+    },
+    placeholder: {
+      en: "Monthly Income",
+      tr: "Aylık Gelir",
+    },
     keyboardType: "numeric",
   },
   {
     id: 3,
-    title: "Enter your monthly expenses",
-    placeholder: "Monthly Expenses",
+    title: {
+      en: "Enter your monthly expenses",
+      tr: "Aylık giderlerinizi girin",
+    },
+    placeholder: {
+      en: "Monthly Expenses",
+      tr: "Aylık Gider",
+    },
     keyboardType: "numeric",
   },
   {
     id: 4,
-    title: "Enter your monthly savings goal",
-    placeholder: "Monthly Savings Goal",
+    title: {
+      en: "Enter your monthly savings goal",
+      tr: "Aylık tasarruf hedefinizi girin",
+    },
+    placeholder: {
+      en: "Monthly Savings Goal",
+      tr: "Aylık Tasarruf Hedefi",
+    },
     keyboardType: "numeric",
   },
 ];
