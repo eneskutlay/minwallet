a86d8744b0ffa55ed9c4992e2104ca0a741d0580
Enes Kutlay <hi@eneskutlay.dev>
2023-10-07 16:04:41 +0300

    Update root page

--- app/index.js
+++ app/index.js
@@ -1,25 +1,13 @@
 import React from "react";
 import { StatusBar, SafeAreaView, StyleSheet, View } from "react-native";
-import { Title } from "../src/components/Texts";
-import PrimaryButton from "../src/components/Buttons";
-import Onboarding from "../src/container/Onboarding";
-import { getAllData, clearAllData } from "../src/lib/storage";
+import OnboardingPage from "./onboarding";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="auto" backgroundColor="#1C1C1E" />
       <View style={styles.header}>
-        <Title>Welcome to Minwallet</Title>
-      </View>
-      <View style={styles.body}>
-        <View style={styles.userData}>
-          <Onboarding />
-        </View>
-      </View>
-      <View style={styles.footer}>
-        <PrimaryButton title="Tüm verileri al" onPress={getAllData} />
-        <PrimaryButton title="Tüm verileri sil" onPress={clearAllData} />
+        <OnboardingPage />
       </View>
     </SafeAreaView>
   );
@@ -33,31 +21,5 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "100%",
     backgroundColor: "#1C1C1E",
-  },
-  header: {
-    width: "100%",
-    alignItems: "center",
-    height: "auto",
-    padding: 20,
-  },
-  body: {
-    flex: 5,
-    width: "100%",
-    alignItems: "center",
-    height: "auto",
-  },
-  userData: {
-    width: "100%",
-    alignItems: "center",
-    height: "auto",
-  },
-  footer: {
-    flex: 2,
-    flexDirection: "row",
-    width: "100%",
-    justifyContent: "space-evenly",
-    alignItems: "center",
-    height: "auto",
-    padding: 14,
   },
 });
