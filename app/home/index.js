a960e739ae5acfd6783323a169a0cfb44ac4425c
Enes Kutlay <hi@eneskutlay.dev>
2023-10-09 18:25:19 +0300

    Data state

--- app/home/index.js
+++ app/home/index.js
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, View } from "react-native";
 import { getLocales } from "expo-localization";
 import PrimaryButton from "../../src/components/Buttons";
 import { Title } from "../../src/components/Texts";
 import { clearAllData } from "../../src/lib/storage";
 import translations from "../../src/lib/lang/translations.json";
+import { getData } from "../../src/lib/storage";
 
 export default function Home({ userData }) {
+  const [userName, setUserName] = useState(null);
   const currentLocale = getLocales()[0].languageCode;
+
+  useEffect(() => {
+    async function fetchUserName() {
+      // Veriyi almadan önce 4 saniye bekleyin
+      const username = await getData("Kullanıcı Adı");
+      setUserName(username);
+    }
+
+    fetchUserName();
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Title>{translations[currentLocale].homeTitle}</Title>
       </View>
-      <View style={styles.body}></View>
+      <View style={styles.body}>
+        {userName ? (
+          <Title>{userName}</Title>
+        ) : (
+          <Title>Kullanıcı adı yükleniyor...</Title>
+        )}
+      </View>
       <View style={styles.footer}>
         <PrimaryButton title="Tüm verileri sil" onPress={clearAllData} />
       </View>
