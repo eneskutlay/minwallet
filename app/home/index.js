8d89802c7b9ea5c5b06ddbaef9a250012b407f11
Enes Kutlay <hi@eneskutlay.dev>
2023-10-31 16:30:30 +0300

    Update

--- app/home/index.js
+++ app/home/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, View, ScrollView, Button } from "react-native";
+import { StyleSheet, View, ScrollView } from "react-native";
 import { getLocales } from "expo-localization";
 import { Title } from "../../src/components/Texts";
 import translations from "../../src/lib/lang/translations.json";
@@ -20,7 +20,7 @@ export default function Home() {
       <ScrollView style={styles.scrollBody}>
         <View style={styles.body}>
           <Card
-            tag='income'
+            tag="income"
             title={
               translations[currentLocale].home.monthlyIncome +
               " " +
@@ -30,7 +30,7 @@ export default function Home() {
             assetImage={require("../../assets/bag.png")}
           />
           <Card
-            tag='expense'
+            tag="expense"
             title={
               translations[currentLocale].home.monthlyExpense +
               " " +
@@ -44,7 +44,7 @@ export default function Home() {
             }
           />
           <Card
-            tag='saving'
+            tag="saving"
             title={
               translations[currentLocale].home.monthlySaving +
               " " +
@@ -53,11 +53,11 @@ export default function Home() {
             description="Est dolore enim ex culpa laborum pariatur officia labore"
             assetImage={require("../../assets/saving.png")}
           />
-          <RecapCard  
-          tag='recap'
-         title='Recap'
-        description="Recap for your overall report and financial freedom analysis"
-        assetImage={require("../../assets/saving.png")}
+          <RecapCard
+            tag="recap"
+            title="Recap"
+            description="Recap for your overall report and financial freedom analysis"
+            assetImage={require("../../assets/saving.png")}
           />
         </View>
       </ScrollView>
