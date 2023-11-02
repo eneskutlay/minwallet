6845487e0313818a94cb6bac399633797b54114b
Enes Kutlay <hi@eneskutlay.dev>
2023-11-02 16:26:07 +0300

    Update layout

--- app/_layout.js
+++ app/_layout.js
@@ -1,4 +1,6 @@
 import { Stack } from "expo-router";
+import Home from "./home/index";
+import App from "./index";
 
 export default function Layout() {
   return (
@@ -7,6 +9,6 @@ export default function Layout() {
         // hide the header bar and hide the bottom tab bar
         headerShown: false,
       }}
-    />
+    ></Stack>
   );
 }
