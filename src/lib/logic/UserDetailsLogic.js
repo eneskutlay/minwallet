96a8aa9e85aba17017f41b608e1719ae3e18f6c9
Enes Kutlay <hi@eneskutlay.dev>
2023-12-09 16:10:51 +0300

    fix save problem

--- src/lib/logic/UserDetailsLogic.js
+++ src/lib/logic/UserDetailsLogic.js
@@ -12,75 +12,72 @@ const useUserDetailsLogic = () => {
   const [dataLoaded, setDataLoaded] = useState(false);
 
   useEffect(() => {
-    // monthly income details keys are: salary, rent, investment, other
-    // monthly expense details keys are: basic, personal, financial, other
-    // monthly savings details keys are: emergency, retirement, investment, other
-
     const fetchData = async () => {
-      const monthlyIncomeSalary = await getData("MonthlyIncomeSalary");
-      const monthlyIncomeRent = await getData("MonthlyIncomeRent");
-      const monthlyIncomeInvestment = await getData("MonthlyIncomeInvestment");
-      const monthlyIncomeOther = await getData("MonthlyIncomeOther");
+      try {
+        const monthlyIncomeSalary = await getData("MonthlyIncomeSalary");
+        const monthlyIncomeRent = await getData("MonthlyIncomeRent");
+        const monthlyIncomeInvestment = await getData(
+          "MonthlyIncomeInvestment"
+        );
+        const monthlyIncomeOther = await getData("MonthlyIncomeOther");
 
-      const monthlyExpenseBasic = await getData("MonthlyExpenseBasic");
-      const monthlyExpensePersonal = await getData("MonthlyExpensePersonal");
-      const monthlyExpenseFinancial = await getData("MonthlyExpenseFinancial");
-      const monthlyExpenseOther = await getData("MonthlyExpenseOther");
+        const monthlyExpenseBasic = await getData("MonthlyExpenseBasic");
+        const monthlyExpensePersonal = await getData("MonthlyExpensePersonal");
+        const monthlyExpenseFinancial = await getData(
+          "MonthlyExpenseFinancial"
+        );
+        const monthlyExpenseOther = await getData("MonthlyExpenseOther");
 
-      const monthlySavingsEmergency = await getData("MonthlySavingsEmergency");
-      const monthlySavingsRetirement = await getData(
-        "MonthlySavingsRetirement"
-      );
-      const monthlySavingsInvestment = await getData(
-        "MonthlySavingsInvestment"
-      );
-      const monthlySavingsOther = await getData("MonthlySavingsOther");
+        const monthlySavingsEmergency = await getData(
+          "MonthlySavingsEmergency"
+        );
+        const monthlySavingsRetirement = await getData(
+          "MonthlySavingsRetirement"
+        );
+        const monthlySavingsInvestment = await getData(
+          "MonthlySavingsInvestment"
+        );
+        const monthlySavingsOther = await getData("MonthlySavingsOther");
 
-      if (
-        monthlyIncomeSalary != null &&
-        monthlyIncomeSalary !== "" &&
-        monthlyIncomeRent != null &&
-        monthlyIncomeRent !== "" &&
-        monthlyIncomeInvestment != null &&
-        monthlyIncomeInvestment !== "" &&
-        monthlyIncomeOther != null &&
-        monthlyIncomeOther !== "" &&
-        monthlyExpenseBasic != null &&
-        monthlyExpenseBasic !== "" &&
-        monthlyExpensePersonal != null &&
-        monthlyExpensePersonal !== "" &&
-        monthlyExpenseFinancial != null &&
-        monthlyExpenseFinancial !== "" &&
-        monthlyExpenseOther != null &&
-        monthlyExpenseOther !== "" &&
-        monthlySavingsEmergency != null &&
-        monthlySavingsEmergency !== "" &&
-        monthlySavingsRetirement != null &&
-        monthlySavingsRetirement !== "" &&
-        monthlySavingsInvestment != null &&
-        monthlySavingsInvestment !== "" &&
-        monthlySavingsOther != null &&
-        monthlySavingsOther !== ""
-      ) {
-        setUserMonthlyIncomeDetails({
-          monthlyIncomeSalary,
-          monthlyIncomeRent,
-          monthlyIncomeInvestment,
-          monthlyIncomeOther,
-        });
-        setUserMonthlyExpenseDetails({
-          monthlyExpenseBasic,
-          monthlyExpensePersonal,
-          monthlyExpenseFinancial,
-          monthlyExpenseOther,
-        });
-        setUserMonthlySavingsDetails({
-          monthlySavingsEmergency,
-          monthlySavingsRetirement,
-          monthlySavingsInvestment,
-          monthlySavingsOther,
-        });
-        setDataLoaded(true);
+        if (
+          monthlyIncomeSalary != null &&
+          monthlyIncomeRent != null &&
+          monthlyIncomeInvestment != null &&
+          monthlyIncomeOther != null &&
+          monthlyExpenseBasic != null &&
+          monthlyExpensePersonal != null &&
+          monthlyExpenseFinancial != null &&
+          monthlyExpenseOther != null &&
+          monthlySavingsEmergency != null &&
+          monthlySavingsRetirement != null &&
+          monthlySavingsInvestment != null &&
+          monthlySavingsOther != null
+        ) {
+          setUserMonthlyIncomeDetails({
+            monthlyIncomeSalary,
+            monthlyIncomeRent,
+            monthlyIncomeInvestment,
+            monthlyIncomeOther,
+          });
+
+          setUserMonthlyExpenseDetails({
+            monthlyExpenseBasic,
+            monthlyExpensePersonal,
+            monthlyExpenseFinancial,
+            monthlyExpenseOther,
+          });
+
+          setUserMonthlySavingsDetails({
+            monthlySavingsEmergency,
+            monthlySavingsRetirement,
+            monthlySavingsInvestment,
+            monthlySavingsOther,
+          });
+
+          setDataLoaded(true);
+        }
+      } catch (error) {
+        console.error("getData error:", error);
       }
     };
 
