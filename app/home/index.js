8bbaf42447eb6a5af753d609af75bd3e047b1730
Enes Kutlay <hi@eneskutlay.dev>
2023-12-18 15:38:55 +0300

    Refactor

--- app/home/index.js
+++ app/home/index.js
@@ -1,75 +1,77 @@
 import React from "react";
-import {
-  StyleSheet,
-  View,
-  ScrollView,
-  Button,
-  SafeAreaView,
-} from "react-native";
+import { StyleSheet, View, Button, SafeAreaView, FlatList } from "react-native";
 import { getLocales } from "expo-localization";
 import { Title } from "../../src/components/Texts";
 import { clearAllData, getAllData } from "../../src/lib/storage";
 import translations from "../../src/lib/lang/translations.json";
 import useUserDataLogic from "../../src/lib/logic/UserDataLogic";
 import Card from "../../src/components/Card";
-import RecapCard from "../../src/components/RecapCard";
 
 export default function Home() {
   const { userData } = useUserDataLogic();
   const currentLocale = getLocales()[0].languageCode;
+  const currentTranslations = translations[currentLocale];
+  const { monthlyIncome, monthlyExpense, monthlySavings } = userData;
+
+  const data = [
+    {
+      tag: "income",
+      title: `${currentTranslations.home.monthlyIncome} ${monthlyIncome}`,
+      description: "Est dolore enim ex culpa laborum pariatur officia labore",
+      assetImage: require("../../assets/bag.png"),
+    },
+    {
+      tag: "expense",
+      title: `${currentTranslations.home.monthlyExpense} ${monthlyExpense}`,
+      description: "Est dolore enim ex culpa laborum pariatur officia labore",
+      assetImage:
+        currentLocale === "en"
+          ? require("../../assets/expense.png")
+          : require("../../assets/expense-tr.png"),
+    },
+    {
+      tag: "savings",
+      title: `${currentTranslations.home.monthlySavings} ${monthlySavings}`,
+      description: "Est dolore enim ex culpa laborum pariatur officia labore",
+      assetImage: require("../../assets/saving.png"),
+    },
+    {
+      tag: "recap",
+      title: "Recap",
+      description:
+        "Recap for your overall report and financial freedom analysis",
+      assetImage: require("../../assets/saving.png"),
+    },
+  ];
+
+  const renderItem = ({ item }) => (
+    <Card
+      tag={item.tag}
+      title={item.title}
+      description={item.description}
+      assetImage={item.assetImage}
+    />
+  );
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
         <Title>
-          {translations[currentLocale].home.welcome} {userData.userName}
+          {currentTranslations.home.welcome} {userData.userName}
         </Title>
       </View>
-      <ScrollView style={styles.scrollBody}>
-        <View style={styles.body}>
-          <Card
-            tag="income"
-            title={
-              translations[currentLocale].home.monthlyIncome +
-              " " +
-              userData.monthlyIncome
-            }
-            description="Est dolore enim ex culpa laborum pariatur officia labore"
-            assetImage={require("../../assets/bag.png")}
-          />
-          <Card
-            tag="expense"
-            title={
-              translations[currentLocale].home.monthlyExpense +
-              " " +
-              userData.monthlyExpense
-            }
-            description="Est dolore enim ex culpa laborum pariatur officia labore"
-            assetImage={
-              currentLocale === "en"
-                ? require("../../assets/expense.png")
-                : require("../../assets/expense-tr.png")
-            }
-          />
-          <Card
-            tag="savings"
-            title={
-              translations[currentLocale].home.monthlySavings +
-              " " +
-              userData.monthlySavings
-            }
-            description="Est dolore enim ex culpa laborum pariatur officia labore"
-            assetImage={require("../../assets/saving.png")}
-          />
-          <RecapCard
-            tag="recap"
-            title="Recap"
-            description="Recap for your overall report and financial freedom analysis"
-            assetImage={require("../../assets/saving.png")}
-          />
-        </View>
-      </ScrollView>
-      <Button title="Clear All Data" onPress={clearAllData} />
-      <Button title="Get All Data" onPress={getAllData} />
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.tag}
+        contentContainerStyle={styles.body}
+        showsVerticalScrollIndicator={false}
+        ItemSeparatorComponent={() => <View style={{ height: 24 }} />}
+      />
+      <View style={styles.buttonsContainer}>
+        <Button title="Clear All Data" onPress={clearAllData} />
+        <Button title="Get All Data" onPress={getAllData} />
+      </View>
     </SafeAreaView>
   );
 }
@@ -77,30 +79,19 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    width: "100%",
-    height: "100%",
     backgroundColor: "#1C1C1E",
   },
   header: {
-    width: "100%",
     alignItems: "center",
-    height: "auto",
     paddingTop: 28,
-    zIndex: 1,
-  },
-  scrollBody: {
-    flex: 0,
-    width: "100%",
-    height: "auto",
   },
   body: {
-    flex: 1,
-    width: "100%",
     alignItems: "center",
-    height: "auto",
-    gap: 24,
     paddingVertical: 16,
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginBottom: 20,
   },
 });
