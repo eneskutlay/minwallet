b3c0597ce7994c490e319bd0730954a03fdfbe31
Enes Kutlay <hi@eneskutlay.dev>
2023-12-18 15:38:45 +0300

    remove slug details

--- app/details/[slug].js
+++ app/details/[slug].js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React from "react";
 import {
   StyleSheet,
   Text,
@@ -10,41 +10,18 @@ import { useLocalSearchParams } from "expo-router";
 import { useRouter } from "expo-router";
 import { getLocales } from "expo-localization";
 import translations from "../../src/lib/lang/translations.json";
-import ManageDetails from "../../src/components/ManageDetails";
-import UserDetailsBySlug from "../../src/components/UserDetailsBySlug";
 
 export default function DetailsPage() {
   const router = useRouter();
   const { slug } = useLocalSearchParams();
   const currentLocale = getLocales()[0].languageCode;
 
-  const translationKey = useMemo(() => {
-    if (slug === "income") {
-      return "income";
-    } else if (slug === "expense") {
-      return "expense";
-    } else if (slug === "savings") {
-      return "savings";
-    }
-    return "defaultKey";
-  }, [slug]);
-
-  const translatedText = useMemo(() => {
-    return translations[currentLocale]["details"][translationKey];
-  }, [currentLocale, translationKey]);
-
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.content}>
-        <View style={styles.head}>
-          <Text style={styles.head}>{translatedText}</Text>
-        </View>
-        <View style={styles.detailList}>
-          <UserDetailsBySlug slug={slug} />
-        </View>
-        <View style={styles.detailContainer}>
-          <ManageDetails />
-        </View>
+      <View style={styles.head}>
+        <Text style={styles.heading}>
+          {translations[currentLocale]["details"][slug]}
+        </Text>
       </View>
       <View style={styles.footer}>
         <TouchableOpacity onPress={() => router.push("/home")}>
@@ -57,38 +34,29 @@ export default function DetailsPage() {
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "#1C1C1E",
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    flexDirection: "column",
+    justifyContent: "space-between",
+  },
+  body: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "space-between",
+  },
+  heading: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10,
+    color: "white",
+  },
+  footer: {
+    padding: 10,
     backgroundColor: "#1C1C1E",
   },
-  content: {
-    flex: 1,
-    alignItems: "center",
-    paddingTop: 28,
-    //justifyContent: "center",
-    padding: 20,
-  },
-  head: {
-    fontSize: 20,
-    fontWeight: "bold",
-    color: "#FFF",
-  },
-  detailList: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 20,
-    marginVertical: 20,
-  },
-  detailContainer: {
-    flex: 1,
-    alignItems: "center",
-  },
   button: {
-    fontSize: 14,
-    marginVertical: 10,
-    color: "green",
-    fontWeight: "bold",
+    textAlign: "center",
+    color: "#fff",
+    fontSize: 20,
   },
 });
